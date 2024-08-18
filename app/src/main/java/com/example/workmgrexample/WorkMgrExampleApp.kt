66dcaad0893dc5fbf9b2d1de20c1bb97d7e18d7d
Enel Almonte <enel_almonte@ucne.edu.do>
2024-08-18 15:45:03 -0400

    3. Set the application class and AndroidEntryPoint

--- app/src/main/java/com/example/workmgrexample/WorkMgrExampleApp.kt
+++ app/src/main/java/com/example/workmgrexample/WorkMgrExampleApp.kt
@@ -1,4 +1,8 @@
 package com.example.workmgrexample
 
-class WorkMgrExampleApp {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WorkMgrExampleApp: Application() {
 }
